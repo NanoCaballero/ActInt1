80ba06fb7ae6cddf192f07d43e80c11d32bc5376
NanoCaballero <A01722757@tec.mx>
2025-05-21 20:12:43 -0600

    Intento de tests 12

--- test/test_transmission.cpp
+++ test/test_transmission.cpp
@@ -13,14 +13,14 @@
 TEST(PatternTest, Found) {
     std::string t = "ABCDEF";
     std::string p = "CDE";
-    auto res = containsPattern(t, p);
+    auto res = contains_Pattern(t, p);
     EXPECT_TRUE(res.first);
     EXPECT_EQ(res.second, 3);
 }
 
 TEST(PalindromeTest, Basic) {
     std::string t = "ABBA";
-    auto res = longestPalindrome(t);
+    auto res = longest_Palindrome(t);
     EXPECT_EQ(res.first, 1);
     EXPECT_EQ(res.second, 4);
 }
